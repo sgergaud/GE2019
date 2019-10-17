c780f7a3d0b5ffc5db52ead65a2be87d7f96e701
sgergaud <44834921+sgergaud@users.noreply.github.com>
2019-10-17 15:40:14 +0100

    Lab3
    Lab3 from last week

---
+++ Lab3/GE1-2019-2020-master/GE1Examples2019/Assets/Scripts/OrbController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbController : MonoBehaviour
+{
+    public GameObject PlayerController;
+
+
+    void OnTriggerEnter(Collider sphereCollider)
+    {
+        if (this.GetComponent<SphereCollider>().isTrigger)
+        {
+            // GameObject.Find("Cube").GetComponent<TankController>().enabled = false;
+            print("Triggerred");
+        }
+
+    }
+
+    void OnTriggerStay()
+    {
+
+
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
