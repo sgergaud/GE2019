f152611c50c7ec5a2ad4335a4548ce445e6b5f4f
sgergaud <44834921+sgergaud@users.noreply.github.com>
2019-12-02 11:08:01 +0000

    Spawn
    It spawns.
    Next step is make one whale spawn at a time.
    
    Rope physics this afternoon

---
+++ SG_gamengines/Assets/Scripts/WhalesSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhalesSpawn : MonoBehaviour
+{
+    public Rigidbody randomWhale;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Rigidbody clone;
+            clone = Instantiate(randomWhale, transform.position, transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.down * 10);
+        }
+    }
+
+    
+
+
+}
