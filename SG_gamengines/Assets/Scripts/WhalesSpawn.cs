bf3317cfce6d8ebbf678ccf894faabecb73ea4f2
sgergaud <44834921+sgergaud@users.noreply.github.com>
2019-12-03 15:47:08 +0000

    Fixing the spawning
    Trying to get conitnous random rotation from the whales

--- SG_gamengines/Assets/Scripts/WhalesSpawn.cs
+++ SG_gamengines/Assets/Scripts/WhalesSpawn.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class WhalesSpawn : MonoBehaviour
 {
-    public Rigidbody randomWhale;
-
+    //public Rigidbody randomWhale;
+    public float Timer = 3f;
+    public GameObject Whale;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //  Rigidbody clone;
+        // clone = Instantiate(randomWhale, transform.position, transform.rotation);
+        // clone.velocity = transform.TransformDirection(Vector3.down * 10);
+        //}
+
+        Timer -= Time.deltaTime;
+
+        if (Timer <= 0f)
         {
-            Rigidbody clone;
-            clone = Instantiate(randomWhale, transform.position, transform.rotation);
-            clone.velocity = transform.TransformDirection(Vector3.down * 10);
+            GameObject WhaleClone;
+            WhaleClone = Instantiate(Whale, transform.position,
+                 Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))) as GameObject; ;
+            WhaleClone = Whale;
+
+            Timer = 3f;
         }
     }
 
