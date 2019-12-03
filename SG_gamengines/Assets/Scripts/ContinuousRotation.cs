3fe1deceeb41a7f83763d0e9f470f0b715bc682f
sgergaud <44834921+sgergaud@users.noreply.github.com>
2019-12-03 16:24:31 +0000

    Rotation
    Got them to have a new rotation when spawned

---
+++ SG_gamengines/Assets/Scripts/ContinuousRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuousRotation : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(xSpin, ySpin, zSpin);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+                    
+}
--- SG_gamengines/Assets/Scripts/WhalesSpawn.cs
+++ SG_gamengines/Assets/Scripts/WhalesSpawn.cs
@@ -6,24 +6,20 @@ public class WhalesSpawn : MonoBehaviour
     //public Rigidbody randomWhale;
     public float Timer = 3f;
     public GameObject Whale;
+   
+    
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKey(KeyCode.Space))
-        //{
-        //  Rigidbody clone;
-        // clone = Instantiate(randomWhale, transform.position, transform.rotation);
-        // clone.velocity = transform.TransformDirection(Vector3.down * 10);
-        //}
-
         Timer -= Time.deltaTime;
-
+    
         if (Timer <= 0f)
         {
             GameObject WhaleClone;
             WhaleClone = Instantiate(Whale, transform.position,
-                 Quaternion.Euler(0f, 0f, Random.Range(0f, 360f))) as GameObject; ;
-            WhaleClone = Whale;
+                 Quaternion.Euler(Random.Range(0f, 360f),Random.Range(0f, 360f), Random.Range(0f,360f))) as GameObject;
+            
+            
 
             Timer = 3f;
         }
