b19258f5c66889547d833acf8e55c76dfe427fce
sgergaud <44834921+sgergaud@users.noreply.github.com>
2019-12-08 07:41:00 +0000

    Redirection of project

--- SG_gamengines/Assets/Scripts/Psychedelic.cs
+++ SG_gamengines/Assets/Scripts/Psychedelic.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ContinuousRotation : MonoBehaviour
+public class Psychedelic : MonoBehaviour
 {
+
     
     // Start is called before the first frame update
     void Start()
     {
-        transform.rotation = Quaternion.Euler(xSpin, ySpin, zSpin);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        
     }
-                    
 }
